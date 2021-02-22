9eec9406e65c757c0556f355bc9b700a0a62a5ac
“berlinen” <1292393108@qq.com>
2021-02-22 16:05:56 +0800

    feat: home pages;

--- src/containers/home/index.tsx
+++ src/containers/home/index.tsx
@@ -1,3 +1,50 @@
-import React from 'react';
+import React, { useMemo, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Helmet } from 'react-helmet';
+import { getHomeList } from './store/actions';
+import styles from './style.css';
+import WithStyles from '../../withStyle';
+
+type Props = {
+  list: any[],
+  getHomeList: () => void
+}
+
+const Home: React.FC<Props> = (props: Props) => {
+  const { list, getHomeList } = props;
+
+  const getList = useMemo(() =>list.map(item => <div className={styles.item} key={item.id} onClick={() => { onClickItem(item.title) }} >{item.title}</div>), [list]);
+
+  const onClickItem = (title: string) => {
+    console.log('title:', title)
+  };
+
+  useEffect(() => {
+    !list.length && getHomeList();
+  }, [list])
+
+  return (
+    <React.Fragment>
+      <Helmet>
+        <title>首页</title>
+        <meta name="description" content="新闻页面 - 丰富多彩的资讯" />
+      </Helmet>
+      <div className={styles.container}>
+				{getList()}
+			</div>
+    </React.Fragment>
+  )
+}
+
+const mapState = state => ({
+  list: state.home.newsList
+});
+
+const mapDispatch = dispatch => ({
+  getHomeList() {
+    dispatch(getHomeList);
+  }
+});
+
+export default connect(mapState, mapDispatch)(WithStyles(Home, styles));
+
