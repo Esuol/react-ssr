8209c2524401a2e7df3920e641af3743cbc8f098
“berlinen” <1292393108@qq.com>
2021-02-22 17:46:02 +0800

    feat: server template

---
+++ src/server/index.ts
@@ -0,0 +1,4 @@
+import express from 'express';
+import proxy from 'express-http-proxy';
+import { matchRoutes } from 'react-router-config';
+import { render } from './utils';
---
+++ src/server/utils.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import { StaticRouter, Route, Switch } from 'react-router-dom';
+import { renderRoutes } from 'react-router-config';
+import { Provider } from 'react-redux';
+import { Helmet } from 'react-helmet';
+
+export const render = (store, routes, req, context) => {
+  const insertCss = (...styles) => {
+    styles.forEach(style => context.css.push(style._getCss()));
+  }
+
+  const content = renderToString((
+    <Provider store={store}>
+      <StaticRouter location={req.path} context={context}>
+        <Switch>
+          {renderRoutes(routes)}
+        </Switch>
+      </StaticRouter>
+    </Provider>
+  ))
+
+  const helmet = Helmet.renderStatic();
+
+  const cssStr = context.css.length ? context.css.join('\n') : '';
+
+  return `
+    <html>
+     <head>
+      ${helmet.title.toString()}
+      ${helmet.meta.toString()}
+      <style>${cssStr}</style>
+     </head>
+     <body>
+      <div id="root">${content}</div>
+      <script>
+        window.context = {
+          state: ${JSON.stringify(store.getState())}
+        }
+      </script>
+      <script src='/index.js'></script>
+     </body>
+    </html>
+  `
+}
