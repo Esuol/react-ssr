73856bd9eee4ca4f8fd33bd170ef32356551884e
“berlinen” <1292393108@qq.com>
2021-02-24 09:24:25 +0800

    fix: bugs

--- src/server/request.ts
+++ src/server/request.ts
@@ -1,16 +1,16 @@
 import axios from 'axios';
 import config from '../config';
 const path = require('path');
-// const fs = require('fs');
+const fs = require('fs');
 
 const createInstance = req => {
   return {
     get: url => {
       const dataPath = path.join(process.cwd(), 'public', url);
-      //const data = fs.readFileSync(`${dataPath}`, 'utf-8');
-      // return Promise.resolve({
-      //   data: JSON.parse(data)
-      // })
+      const data = fs.readFileSync(`${dataPath}`, 'utf-8');
+      return Promise.resolve({
+        data: JSON.parse(data)
+      })
     }
   }
 }
